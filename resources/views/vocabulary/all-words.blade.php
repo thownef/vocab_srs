4d2d73a9fce746ad40a9c1c92e07503a3af4d984
thownef <vantho3042000@gmail.com>
2025-09-09 19:32:06 +0700

    show search

--- resources/views/vocabulary/all-words.blade.php
+++ resources/views/vocabulary/all-words.blade.php
@@ -15,6 +15,9 @@
                         <label for="search" class="block text-sm font-medium text-gray-700 mb-2">
                             <i class="fas fa-search mr-1"></i>Tìm kiếm
                         </label>
+                        <input type="text" id="search" name="search" value="{{ request('search') }}"
+                            class="w-full px-3 py-2 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-transparent"
+                            placeholder="Tìm theo từ, nghĩa, ví dụ...">
                     </div>
 
                     <div>
